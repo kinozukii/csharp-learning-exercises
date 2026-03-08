feecb010aff813fd48d7f134ffa5e1606f189832
kinozukii <tommyliam4@gmail.com>
2026-03-08 00:24:45 -0300

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to my C# learning project!");
+
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Hello " + name + "!");
+    }
+}
